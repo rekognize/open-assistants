40426267eabb5221582bc29b170c7f620880c950
onur mat <onurmatik@gmail.com>
2024-11-05 22:36:57 +0300

    draft core functions

---
+++ oa/tools/core.py
@@ -0,0 +1,22 @@
+
+
+def get_meta_value(assistant_id, key):
+    pass
+
+
+def write_content(content: str, folder: str, filename: str) -> None:
+    """
+    Write file to S3
+    """
+
+
+def read_read(folder, name):
+    """
+    Read the contents of the given file.
+    """
+
+
+def add_to_vector_store(file):
+    pass
+
+
