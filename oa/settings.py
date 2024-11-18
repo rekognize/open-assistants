ed07bf6074af1b42a9a55e01e374c3709e9c10c1
onur mat <onurmatik@gmail.com>
2024-11-18 22:33:46 +0300

    Define STATIC_ROOT unconditionally

--- oa/settings.py
+++ oa/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 IS_LOCAL = os.getenv("IS_LOCAL", "false").lower() == "true"
 
@@ -119,7 +120,6 @@ if IS_LOCAL:
     # Local settings for static and media files
     STATIC_URL = "/static/"
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
