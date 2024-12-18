b13a7f4cc1c2d82f72339e2f1f8e0b9865008f71
Kemal Soylu <kemalbsoylu@gmail.com>
2024-12-18 14:03:41 +0300

    fixed migration dependency and updated requirements

--- oa/main/migrations/0003_sharedlink.py
+++ oa/main/migrations/0003_sharedlink.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0002_project_is_oa_project'),
+        ('main', '0001_initial'),
     ]
 
     operations = [
