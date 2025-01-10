9b73d75c5be3cbd6f6efb5c5c87515793969a231
Kemal Soylu <kemalbsoylu@gmail.com>
2025-01-10 11:30:00 +0300

    removed unused views for analytics

--- oa/urls.py
+++ oa/urls.py
@@ -18,21 +18,15 @@ urlpatterns = [
     path('', main_views.HomeView.as_view(), name='home'),
     path('<uuid:project_uuid>/', main_views.HomeView.as_view(), name='home'),
 
-    # path('manage/', main_views.manage_assistants, name='manage_assistants'),
     path('<uuid:project_uuid>/manage/', main_views.manage_assistants, name='manage_assistants'),
-
-    # path('chat/', main_views.thread_detail, name='thread_detail'),
+    path('<uuid:project_uuid>/analytics/', main_views.analytics, name='analytics'),
     path('<uuid:project_uuid>/chat/', main_views.thread_detail, name='thread_detail'),
 
-    path('<uuid:project_uuid>/analytics/', main_views.analytics, name='analytics'),
-    path('analytics/thread_data/', main_views.get_assistant_threads, name='get_assistant_threads'),
+    path('shared/<uuid:shared_token>/', main_views.shared_thread_detail, name='shared_thread_detail'),
 
     path('db_threads/', main_views.create_db_thread, name='create_db_thread'),
-    path('db_threads/list/', main_views.list_threads, name='list_threads'),
 
     path('files/image/<str:file_id>/', main_views.serve_image_file, name='serve_image_file'),
-
-    path('shared/<uuid:shared_token>/', main_views.shared_thread_detail, name='shared_thread_detail'),
 ]
 
 admin.site.index_title = settings.SITE_NAME
